e60e3392f43ac615ac2d4e53f3dc942e2b44f46c
Artem.Vasilev <a@a.a>
2026-06-03 12:44:26 +0300

    init

---
+++ AiDesktopAssistant/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AiDesktopAssistant;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ AiDesktopAssistant/Models/ChatMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace AiDesktopAssistant.Models;
+
+public class ChatMessage(string Role, string Content)
+{
+    public string Role { get; set; } = Role;
+    public string Content { get; set; } = Content;
+}
